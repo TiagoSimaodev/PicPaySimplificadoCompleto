63442972484edf4198ab7efcc9a0069cdf3ba69b
Tiago Simao <110579953+TiagoSimaodev@users.noreply.github.com>
2023-09-24 13:27:23 -0300

    feat UserRepository.java

--- src/main/java/com/PicPaySimplificado/repositories/UserRepository.java
+++ src/main/java/com/PicPaySimplificado/repositories/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional <User> findUserByDocument(String document);
-    Optional <User> findUserByDocument(Long id);
+    Optional <User> findUserById(Long id);
 
 }
