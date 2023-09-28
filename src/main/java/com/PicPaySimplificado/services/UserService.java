897f35b1c9045a1e5ed737350a31c03f3f101b39
Tiago Simao <110579953+TiagoSimaodev@users.noreply.github.com>
2023-09-27 22:59:14 -0300

    feat alter UserService

--- src/main/java/com/PicPaySimplificado/services/UserService.java
+++ src/main/java/com/PicPaySimplificado/services/UserService.java
@@ -2,11 +2,13 @@ package com.PicPaySimplificado.services;
 
 import com.PicPaySimplificado.domain.user.User;
 import com.PicPaySimplificado.domain.user.UserType;
+import com.PicPaySimplificado.dtos.UserDTO;
 import com.PicPaySimplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -26,6 +28,17 @@ public class UserService {
     public User findUserById(Long id) throws Exception {
         return  this.repository.findUserById(id).orElseThrow(()-> new Exception("Usuario não encontrado"));
     }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+      return this.repository.findAll();
+    }
+
     public void saveUser(User user){
         this.repository.save(user);
     }
